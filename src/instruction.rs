c5ea2ee95d151a01f9167f2212fd30e594667a94
LS <the413reply@gmail.com>
2025-02-12 02:24:10 +0000

    pach2

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,95 +1,48 @@
 /// ILI9225 instructions.
 #[derive(Debug, Clone, Copy)]
 pub enum Instruction {
-    DRIVER_OUTPUT_CTRL = 0x01,  // Driver Output Control
-    LCD_AC_DRIVING_CTRL = 0x02, // LCD AC Driving Control
-    ENTRY_MODE = 0x03,          // Entry Mode
-    DISP_CTRL1 = 0x07,          // Display Control 1
-    DISP_CTRL2 = 0x08,          // Blank Period Control
-    FRAME_CYCLE_CTRL = 0x0B,    // Frame Cycle Control
-    INTERFACE_CTRL = 0x0C,      // Interface Control
-    OSC_CTRL = 0x0F,            // Osc Control
+    DriverOutputCtrl = 0x01,      // Driver Output Control
+    LcdAcDrivingCtrl = 0x02,     // LCD AC Driving Control
+    EntryMode = 0x03,             // Entry Mode
+    DispCtrl1 = 0x07,             // Display Control 1
+    DispCtrl2 = 0x08,             // Blank Period Control
+    FrameCycleCtrl = 0x0B,          // Frame Cycle Control
+    InterfaceCtrl = 0x0C,         // Interface Control
+    OscCtrl = 0x0F,               // Osc Control
     //*************Power On sequence ****************//
-    POWER_CTRL1 = 0x10,   // Power Control 1
-    POWER_CTRL2 = 0x11,   // Power Control 2
-    POWER_CTRL3 = 0x12,   // Power Control 3
-    POWER_CTRL4 = 0x13,   // Power Control 4
-    POWER_CTRL5 = 0x14,   // Power Control 5
-    VCI_RECYCLING = 0x15, // VCI Recycling
-    RAM_ADDR_SET1 = 0x20, // Horizontal GRAM Address Set
-    RAM_ADDR_SET2 = 0x21, // Vertical GRAM Address Set
-    GRAM_DATA_REG = 0x22, // GRAM Data Register
+    PowerCtrl1 = 0x10,            // Power Control 1
+    PowerCtrl2 = 0x11,            // Power Control 2
+    PowerCtrl3 = 0x12,            // Power Control 3
+    PowerCtrl4 = 0x13,            // Power Control 4
+    PowerCtrl5 = 0x14,            // Power Control 5
+    VciRecycling = 0x15,          // VCI Recycling
+    RamAddrSet1 = 0x20,           // Horizontal GRAM Address Set
+    RamAddRSet2 = 0x21,           // Vertical GRAM Address Set
+    GramDataReg = 0x22,           // GRAM Data Register
     //-------------- Set GRAM area -----------------//
-    GATE_SCAN_CTRL = 0x30,          // Gate Scan Control Register
-    VERTICAL_SCROLL_CTRL1 = 0x31,   // Vertical Scroll Control 1 Register
-    VERTICAL_SCROLL_CTRL2 = 0x32,   // Vertical Scroll Control 2 Register
-    VERTICAL_SCROLL_CTRL3 = 0x33,   // Vertical Scroll Control 3 Register
-    PARTIAL_DRIVING_POS1 = 0x34,    // Partial Driving Position 1 Register
-    PARTIAL_DRIVING_POS2 = 0x35,    // Partial Driving Position 2 Register
-    HORIZONTAL_WINDOW_ADDR1 = 0x36, // Horizontal Address Start Position (Window-HStart)
-    HORIZONTAL_WINDOW_ADDR2 = 0x37, // Horizontal Address End Position   (Window-HEnd)
-    VERTICAL_WINDOW_ADDR1 = 0x38,   // Vertical Address Start Position	  (Window-VStart)
-    VERTICAL_WINDOW_ADDR2 = 0x39,   // Vertical Address End Position	  (Window-VEnd)
+    GateScanCtrl = 0x30,          // Gate Scan Control Register
+    VerticalScrollCtrl1 = 0x31,     // Vertical Scroll Control 1 Register
+    VerticalScrollCtrl2 = 0x32,     // Vertical Scroll Control 2 Register
+    VerticalScrollCtrl3 = 0x33,     // Vertical Scroll Control 3 Register
+    PartialDrivingPos1 = 0x34,      // Partial Driving Position 1 Register
+    PartialDrivingPos2 = 0x35,      // Partial Driving Position 2 Register
+    HorizontalWindowAddr1 = 0x36,   // Horizontal Address Start Position (Window-HStart)
+    HorizontalWindowAddr2 = 0x37,   // Horizontal Address End Position (Window-HEnd)
+    VerticalWindowAddr1 = 0x38,     // Vertical Address Start Position (Window-VStart)
+    VerticalWindowAddr2 = 0x39,     // Vertical Address End Position (Window-VEnd)
     // ----------- Adjust the Gamma Curve ----------//
-    GAMMA_CTRL1 = 0x50,  // Gamma Control 1
-    GAMMA_CTRL2 = 0x51,  // Gamma Control 2
-    GAMMA_CTRL3 = 0x52,  // Gamma Control 3
-    GAMMA_CTRL4 = 0x53,  // Gamma Control 4
-    GAMMA_CTRL5 = 0x54,  // Gamma Control 5
-    GAMMA_CTRL6 = 0x55,  // Gamma Control 6
-    GAMMA_CTRL7 = 0x56,  // Gamma Control 7
-    GAMMA_CTRL8 = 0x57,  // Gamma Control 8
-    GAMMA_CTRL9 = 0x58,  // Gamma Control 9
-    GAMMA_CTRL10 = 0x59, // Gamma Control 10
+    GammaCtrl1 = 0x50,            // Gamma Control 1
+    GammaCtrl2 = 0x51,            // Gamma Control 2
+    GammaCtrl3 = 0x52,            // Gamma Control 3
+    GammaCtrl4 = 0x53,            // Gamma Control 4
+    GammaCtrl5 = 0x54,            // Gamma Control 5
+    GammaCtrl6 = 0x55,            // Gamma Control 6
+    GammaCtrl7 = 0x56,            // Gamma Control 7
+    GammaCtrl8 = 0x57,            // Gamma Control 8
+    GammaCtrl9 = 0x58,            // Gamma Control 9
+    GammaCtrl10 = 0x59,           // Gamma Control 10
 
     // delay 50ms
-    // misc
-    COLOR_BLACK = 0x000000,
-    COLOR_WHITE = 0xFFFFFF,
+    ColorBlack = 0x000000,
+    ColorWhite = 0xFFFFFF,
 }
-// const UINT16  regValues[] =
-// {
-// 	=0x01, =0x011C,
-// 	=0x02, =0x0100,
-// 	=0x03, =0x1030,
-// 	=0x08, =0x0808, // set BP and FP
-// 	=0x0B, =0x1100, // frame cycle
-// 	=0x0C, =0x0000, // RGB interface setting R0Ch==0x0110 for RGB 18Bit and R0Ch=0111for RGB16Bit
-// 	=0x0F, =0x1401, // Set frame rate----0801
-// 	=0x15, =0x0000, // set system interface
-// 	=0x20, =0x0000, // Set GRAM Address
-// 	=0x21, =0x0000, // Set GRAM Address
-// 	//*************Power On sequence ****************//
-// 	TFTLCD_DELAY16, 50, // delay 50ms
-// 	=0x10, =0x0800, // Set SAP,DSTB,STB----0A00
-// 	=0x11, =0x1F3F, // Set APON,PON,AON,VCI1EN,VC----1038
-// 	TFTLCD_DELAY16, 50, // delay 50ms
-// 	=0x12, =0x0121, // Internal reference voltage= Vci;----1121
-// 	=0x13, =0x006F, // Set GVDD----0066
-// 	=0x14, =0x4349, // Set VCOMH/VCOML voltage----5F60
-// 	//-------------- Set GRAM area -----------------//
-// 	=0x30, =0x0000,
-// 	=0x31, =0x00DB,
-// 	=0x32, =0x0000,
-// 	=0x33, =0x0000,
-// 	=0x34, =0x00DB,
-// 	=0x35, =0x0000,
-// 	=0x36, =0x00AF,
-// 	=0x37, =0x0000,
-// 	=0x38, =0x00DB,
-// 	=0x39, =0x0000,
-// 	// ----------- Adjust the Gamma Curve ----------//
-// 	=0x50, =0x0001, // =0x0400
-// 	=0x51, =0x200B, // =0x060B
-// 	=0x52, =0x0000, // =0x0C0A
-// 	=0x53, =0x0404, // =0x0105
-// 	=0x54, =0x0C0C, // =0x0A0C
-// 	=0x55, =0x000C, // =0x0B06
-// 	=0x56, =0x0101, // =0x0004
-// 	=0x57, =0x0400, // =0x0501
-// 	=0x58, =0x1108, // =0x0E00
-// 	=0x59, =0x050C, // =0x000E
-// 	TFTLCD_DELAY16, 50, // delay 50ms
-// 	=0x07, =0x1017,
-// 	=0x22, =0x0000,
-// };
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 #![no_std]
-
 //! This crate provides a ILI9225 driver to connect to TFT displays.
 pub mod instruction;
 use crate::instruction::Instruction;
 use embedded_hal::delay::DelayNs;
 use embedded_hal::digital::OutputPin;
 use embedded_hal::spi;
-
 pub struct ILI9225<SPI, RS, RST>
 where
     SPI: spi::SpiDevice,
@@ -15,26 +13,20 @@ where
 {
     /// SPI
     spi: SPI,
-
     /// Data/command pin.
     rs: RS,
-
     /// Reset pin.
     rst: Option<RST>,
-
     /// Whether the display is RGB (true) or BGR (false)
     rgb: bool,
-
     /// Whether the colours are inverted (true) or not (false)
     inverted: bool,
-
     /// Global image offset
     dx: u16,
     dy: u16,
     width: u32,
     height: u32,
 }
-
 /// Display orientation.
 #[derive(Clone, Copy)]
 pub enum Orientation {
@@ -43,7 +35,6 @@ pub enum Orientation {
     PortraitSwapped = 0x1000,
     LandscapeSwapped = 0x1018,
 }
-
 impl<SPI, RS, RST> ILI9225<SPI, RS, RST>
 where
     SPI: spi::SpiDevice,
@@ -73,7 +64,6 @@ where
         };
         display
     }
-
     pub fn init<DELAY>(&mut self, delay: &mut DELAY) -> Result<(), ()>
     where
         DELAY: DelayNs,
@@ -81,40 +71,60 @@ where
         self.hard_reset(delay)?;
         // Power-on sequence
         //
-        self.write_command(Instruction::POWER_CTRL1, &[0x0000])?;
-        self.write_command(Instruction::POWER_CTRL2, &[0x0000])?;
-        self.write_command(Instruction::POWER_CTRL3, &[0x0000])?;
-        self.write_command(Instruction::POWER_CTRL4, &[0x0000])?;
-        self.write_command(Instruction::POWER_CTRL5, &[0x0000])?;
+        self.write_command(Instruction::PowerCtrl1, &[0x0000])?; //Set SAP,DSTB,STB
+        self.write_command(Instruction::PowerCtrl2, &[0x0000])?; // Set APON,PON,AON,VCI1EN,VC
+        self.write_command(Instruction::PowerCtrl3, &[0x0000])?; // Set BT,DC1,DC2,DC3
+        self.write_command(Instruction::PowerCtrl4, &[0x0000])?; // Set GVDD
+        self.write_command(Instruction::PowerCtrl5, &[0x0000])?; // Set VCOMH/VCOML voltage
         delay.delay_ms(50);
         // Power-on sequence
-        // self.set_register(ILI9225_POWER_CTRL2, 0x0018)
-        self.write_command(Instruction::POWER_CTRL2, &[0x00, 0x18])?;
-        self.write_command(Instruction::POWER_CTRL3, &[0x61, 0x21])?;
-        self.write_command(Instruction::POWER_CTRL4, &[0x00, 0x6F])?;
-        self.write_command(Instruction::POWER_CTRL5, &[0x49, 0x5F])?;
-        self.write_command(Instruction::POWER_CTRL1, &[0x0F, 0x00])?;
+        self.write_command(Instruction::PowerCtrl2, &[0x00, 0x18])?; // Set APON,PON,AON,VCI1EN,VC
+        self.write_command(Instruction::PowerCtrl3, &[0x61, 0x21])?; // Set BT,DC1,DC2,DC3
+        self.write_command(Instruction::PowerCtrl4, &[0x00, 0x6F])?; // Set GVDD  /*007F 0088 */
+        self.write_command(Instruction::PowerCtrl5, &[0x49, 0x5F])?; // Set VCOMH/VCOML voltage
+        self.write_command(Instruction::PowerCtrl1, &[0x08, 0x00])?; // Set SAP,DSTB,STB
         delay.delay_ms(50);
-        self.write_command(Instruction::POWER_CTRL2, &[0x10, 0x3B])?;
+        self.write_command(Instruction::PowerCtrl2, &[0x10, 0x3B])?; // Set APON,PON,AON,VCI1EN,VC
         delay.delay_ms(50);
-        self.write_command(Instruction::DRIVER_OUTPUT_CTRL, &[0x01, 0x1C])?;
-        self.write_command(Instruction::LCD_AC_DRIVING_CTRL, &[0x01, 0x00])?;
-        self.write_command(
-            Instruction::ENTRY_MODE,
-            &[0x10, 0x30, 0x10, 0x28, 0x10, 0x00, 0x10, 0x18],
-        )?;
-        self.write_command(Instruction::DISP_CTRL1, &[0x00, 0x00])?;
-        self.write_command(Instruction::DISP_CTRL2, &[0x08, 0x08])?;
-        self.write_command(Instruction::FRAME_CYCLE_CTRL, &[0x11, 0x00])?;
-        self.write_command(Instruction::INTERFACE_CTRL, &[0x00, 0x00])?;
-        self.write_command(Instruction::OSC_CTRL, &[0x0D, 0x01])?;
-        self.write_command(Instruction::VCI_RECYCLING, &[0x00, 0x20])?;
+        self.write_command(Instruction::DriverOutputCtrl, &[0x01, 0x1C])?; // set the display line number and display direction
+        self.write_command(Instruction::LcdAcDrivingCtrl, &[0x01, 0x00])?; // set 1 line inversion
+        self.write_command(Instruction::EntryMode, &[0x10, 0x30])?; // Set GRAM write direction and BGR=1.
+        self.write_command(Instruction::DispCtrl1, &[0x00, 0x00])?; // Display off
+        self.write_command(Instruction::DispCtrl2, &[0x08, 0x08])?; // Set the back porch and front porch
+        self.write_command(Instruction::FrameCycleCtrl, &[0x11, 0x00])?; // Set the clocks number per line
+        self.write_command(Instruction::InterfaceCtrl, &[0x00, 0x00])?; // CPU interface
+        self.write_command(Instruction::OscCtrl, &[0x0D, 0x01])?; // Set Osc
+        self.write_command(Instruction::VciRecycling, &[0x00, 0x20])?; // Set VCI recycling
+        self.write_command(Instruction::RamAddrSet1, &[0x00, 0x00])?; // RAM Address
+        self.write_command(Instruction::RamAddRSet2, &[0x00, 0x00])?; // RAM Address
+        delay.delay_ms(20);
+        //-------------- Set GRAM area -----------------//
+        self.write_command(Instruction::GateScanCtrl, &[0x00, 0x00])?;
+        self.write_command(Instruction::VerticalScrollCtrl1, &[0x00, 0xDB])?;
+        self.write_command(Instruction::VerticalScrollCtrl2, &[0x00, 0x00])?;
+        self.write_command(Instruction::VerticalScrollCtrl3, &[0x00, 0x00])?; //0x0000
+        self.write_command(Instruction::PartialDrivingPos1, &[0x00, 0xDB])?; //0x00DB
+        self.write_command(Instruction::PartialDrivingPos2, &[0x00, 0x00])?; //0x0000
+        self.write_command(Instruction::HorizontalWindowAddr1, &[0x00, 0xAF])?; //0x00AF
+        self.write_command(Instruction::HorizontalWindowAddr2, &[0x00, 0x00])?; //0x0000
+        self.write_command(Instruction::VerticalWindowAddr1, &[0x00, 0xDB])?; //0x00DB
+        self.write_command(Instruction::VerticalWindowAddr2, &[0x00, 0x00])?; //0x0000
+        // ----------- Adjust the Gamma Curve ----------//
+        self.write_command(Instruction::GammaCtrl1, &[0x00, 0x00])?; //0x0000
+        self.write_command(Instruction::GammaCtrl2, &[0x08, 0x08])?; //0x0808
+        self.write_command(Instruction::GammaCtrl3, &[0x08, 0x0A])?; //0x080A
+        self.write_command(Instruction::GammaCtrl4, &[0x00, 0x0A])?; //0x000A
+        self.write_command(Instruction::GammaCtrl5, &[0x0A, 0x08])?; //0x0A08
+        self.write_command(Instruction::GammaCtrl6, &[0x08, 0x08])?; //0x0808
+        self.write_command(Instruction::GammaCtrl7, &[0x00, 0x00])?; //0x0000
+        self.write_command(Instruction::GammaCtrl8, &[0x0A, 0x00])?; //0x0A00
+        self.write_command(Instruction::GammaCtrl9, &[0x07, 0x10])?;
+        self.write_command(Instruction::GammaCtrl10, &[0x07, 0x10])?;
+        self.write_command(Instruction::DispCtrl1, &[0x00, 0x12])?;
         delay.delay_ms(50);
-        self.write_command(Instruction::DISP_CTRL1, &[0x10, 0x17])?;
-        delay.delay_ms(200);
+        self.write_command(Instruction::DispCtrl1, &[0x10, 0x17])?;
         Ok(())
     }
-
     pub fn hard_reset<DELAY>(&mut self, delay: &mut DELAY) -> Result<(), ()>
     where
         DELAY: DelayNs,
@@ -129,6 +139,7 @@ where
         Ok(())
     }
     fn write_command(&mut self, command: Instruction, params: &[u8]) -> Result<(), ()> {
+        // 发送命令给 LCD 控制器的功能。  它还具备发送命令参数的能力
         self.rs.set_low().map_err(|_| ())?;
         self.spi.write(&[command as u8]).map_err(|_| ())?;
         if !params.is_empty() {
@@ -137,20 +148,16 @@ where
         }
         Ok(())
     }
-
     fn start_data(&mut self) -> Result<(), ()> {
         self.rs.set_high().map_err(|_| ())
     }
-
     fn write_data(&mut self, data: &[u8]) -> Result<(), ()> {
         self.spi.write(data).map_err(|_| ())
     }
-
     /// Writes a data word to the display.
     fn write_word(&mut self, value: u16) -> Result<(), ()> {
         self.write_data(&value.to_be_bytes())
     }
-
     fn write_words_buffered(&mut self, words: impl IntoIterator<Item = u16>) -> Result<(), ()> {
         let mut buffer = [0; 32];
         let mut index = 0;
@@ -166,47 +173,43 @@ where
         }
         self.write_data(&buffer[0..index])
     }
-
+    //
     // pub fn set_orientation(&mut self, orientation: &Orientation) -> Result<(), ()> {
     //     if self.rgb {
     //         self.write_command(Instruction::MADCTL, &[*orientation as u8])?;
     //     } else {
-    //         self.write_command(Instruction::MADCTL, &[*orientation as u8 | 0x08])?;
+    //         self.write_command(Instruction::MADCTL, &[orientation as u8 | 0x08])?;
     //     }
     //     Ok(())
     // }
-
     /// Sets the global offset of the displayed image
     pub fn set_offset(&mut self, dx: u16, dy: u16) {
         self.dx = dx;
         self.dy = dy;
     }
-
     /// Sets the address window for the display.
     ///
     pub fn set_address_window(&mut self, sx: u16, sy: u16, ex: u16, ey: u16) -> Result<(), ()> {
-        self.write_command(Instruction::GATE_SCAN_CTRL, &[])?;
-        self.write_command(Instruction::RAM_ADDR_SET1, &[])?;
+        self.write_command(Instruction::GateScanCtrl, &[])?;
+        self.write_command(Instruction::RamAddrSet1, &[])?;
         self.start_data()?;
         self.write_word(sx + self.dx)?;
         self.write_word(ex + self.dx)?;
-        self.write_command(Instruction::RAM_ADDR_SET2, &[])?;
+        self.write_command(Instruction::RamAddRSet2, &[])?;
         self.start_data()?;
         self.write_word(sy + self.dy)?;
         self.write_word(ey + self.dy)
     }
-
     /// Sets a pixel color at the given coords.
     pub fn set_pixel(&mut self, x: u16, y: u16, color: u16) -> Result<(), ()> {
         self.set_address_window(x, y, x, y)?;
-        self.write_command(Instruction::GRAM_DATA_REG, &[])?;
+        self.write_command(Instruction::GramDataReg, &[])?;
         self.start_data()?;
         self.write_word(color)
     }
-
     /// Writes pixel colors sequentially into the current drawing window
     pub fn write_pixels<P: IntoIterator<Item = u16>>(&mut self, colors: P) -> Result<(), ()> {
-        self.write_command(Instruction::GRAM_DATA_REG, &[])?;
+        self.write_command(Instruction::GramDataReg, &[])?;
         self.start_data()?;
         for color in colors {
             self.write_word(color)?;
@@ -217,11 +220,10 @@ where
         &mut self,
         colors: P,
     ) -> Result<(), ()> {
-        self.write_command(Instruction::GRAM_DATA_REG, &[])?;
+        self.write_command(Instruction::GramDataReg, &[])?;
         self.start_data()?;
         self.write_words_buffered(colors)
     }
-
     /// Sets pixel colors at the given drawing window
     pub fn set_pixels<P: IntoIterator<Item = u16>>(
         &mut self,
@@ -234,7 +236,6 @@ where
         self.set_address_window(sx, sy, ex, ey)?;
         self.write_pixels(colors)
     }
-
     pub fn set_pixels_buffered<P: IntoIterator<Item = u16>>(
         &mut self,
         sx: u16,
@@ -247,6 +248,7 @@ where
         self.write_pixels_buffered(colors)
     }
 }
+
 
 #[cfg(feature = "graphics")]
 extern crate embedded_graphics_core;
@@ -262,10 +264,10 @@ use self::embedded_graphics_core::{
 };
 
 #[cfg(feature = "graphics")]
-impl<SPI, DC, RST> DrawTarget for ILI9225<SPI, DC, RST>
+impl<SPI, RS, RST> DrawTarget for ILI9225<SPI, RS, RST>
 where
     SPI: spi::SpiDevice,
-    DC: OutputPin,
+    RS: OutputPin,
     RST: OutputPin,
 {
     type Error = ();
@@ -329,10 +331,10 @@ where
 }
 
 #[cfg(feature = "graphics")]
-impl<SPI, DC, RST> OriginDimensions for ILI9225<SPI, DC, RST>
+impl<SPI, RS, RST> OriginDimensions for ILI9225<SPI, RS, RST>
 where
     SPI: spi::SpiDevice,
-    DC: OutputPin,
+    RS: OutputPin,
     RST: OutputPin,
 {
     fn size(&self) -> Size {
